c2aed14736268105e5a323d9d916cffafef2f09a
Indu <indubalaselvaraj@gmail.com>
2025-03-26 21:56:21 +0530

    update cors options..

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   preflightContinue: false,
-  optionsSuccessStatus: 204,
+  optionsSuccessStatus: 200,
 };
 
 // Apply CORS with options
