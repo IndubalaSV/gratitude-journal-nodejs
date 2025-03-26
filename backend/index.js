c270dffcba4c7e47aa56c591f7c78e1f632efd62
Indu <indubalaselvaraj@gmail.com>
2025-03-26 21:07:11 +0530

    enable preflight for all routes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = express();
 
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
